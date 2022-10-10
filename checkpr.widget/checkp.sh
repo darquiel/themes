ac6f597d1500cd6d28c01fe03956d538794134bf
mja <vamp_paradox@hotmail.com>
2022-10-10 11:14:59 -0700

    minor edits to accommodate the path issues.

--- checkpr.widget/checkp.sh
+++ checkpr.widget/checkp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-pushd
-var_pr=$`gh pr list --serach release/rover/3.32`
+pushd ~/server/SRC/skel
+var_pr=$`gh pr list --search release/rover/3.32`
+popd
 if [ ${#var_pr} = 1 ] 
 then cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
 else cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
