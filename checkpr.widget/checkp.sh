dd73bedf52b42d96d7059e96bd7db50bef089bbe
mja <mark.andrews@serverobotics.com>
2022-10-11 10:34:48 -0700

    Changes for debugging.

--- checkpr.widget/checkp.sh
+++ checkpr.widget/checkp.sh
@@ -6,9 +6,9 @@ if [ ${#var_pr} = 1 ]
 then 
     rm -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
     cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/status.png
-    echo "no open PR's"
+    echo "no open PR's" >> ~/Ubersicht/checkpr.widget/logfile.txt
 else 
     rm -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
     cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.widget/resources/status.png
-    echo "Open PR's"
+    echo "Open PR's" >> ~/Ubersicht/checkpr.widget/logfile.txt
 fi
