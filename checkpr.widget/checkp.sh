165564f42ea9e3043a4161861ccb6a19e7f0d305
mja <mark.andrews@serverobotics.com>
2022-10-11 09:48:31 -0700

    Modifications for testing.

--- checkpr.widget/checkp.sh
+++ checkpr.widget/checkp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 pushd ~/serve/SRC/skel
 var_pr=$`gh pr list --search mja/base` 
-popd ; 
+popd
 if [ ${#var_pr} = 1 ] 
-    then cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png 
-    else cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
+then 
+    cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png 
+else 
+    cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
 fi
