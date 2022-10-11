188c921e04da9338d400d3818294e222d20ce4f5
mja <mark.andrews@serverobotics.com>
2022-10-11 10:06:05 -0700

    Cleaning up images

--- checkpr.widget/checkp.sh
+++ checkpr.widget/checkp.sh
@@ -4,7 +4,11 @@ var_pr=$`gh pr list --search base:mja/base`
 popd
 if [ ${#var_pr} = 1 ] 
 then 
-    cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png 
+    rm -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
+    cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
+    echo "no open PR's"
 else 
+    rm -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
     cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
+    echo "Open PR's"
 fi
