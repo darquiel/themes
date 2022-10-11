88dd64aaac75305094544bdf98eb3b1295943d5e
mja <mark.andrews@serverobotics.com>
2022-10-11 10:13:39 -0700

    Further testing

--- checkpr.widget/checkp.sh
+++ checkpr.widget/checkp.sh
@@ -5,10 +5,10 @@ popd
 if [ ${#var_pr} = 1 ] 
 then 
     rm -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
-    cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
+    cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/status.png
     echo "no open PR's"
 else 
     rm -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
-    cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
+    cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.widget/resources/status.png
     echo "Open PR's"
 fi
