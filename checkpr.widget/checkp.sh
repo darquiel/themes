853b3f6e89a15c70c714d2819142922b57f5e4c3
mja <vamp_paradox@hotmail.com>
2022-10-10 22:52:01 -0700

    Additional icons, and testing modifications.

--- checkpr.widget/checkp.sh
+++ checkpr.widget/checkp.sh
@@ -3,6 +3,6 @@ pushd ~/serve/SRC/skel
 var_pr=$`gh pr list --search release/rover/3.32` 
 popd ; 
 if [ ${#var_pr} = 1 ] 
-    then cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png 
-    else cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
+    then cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png 
+    else cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
 fi
