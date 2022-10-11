f33e3cd28eb2d096915977bd9e84d8e6e7a5e425
mja <mark.andrews@serverobotics.com>
2022-10-11 11:58:24 -0700

    Corrected a typo

--- checkpr.widget/checkp.sh
+++ checkpr.widget/checkp.sh
@@ -3,7 +3,7 @@ var_pr=$`/opt/homebrew/bin/gh pr list -R serve-robotics/x --search base:release/
 if [ ${#var_pr} = 1 ] 
 then 
     #rm -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
-    cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/status.pngs
+    cp ~/Ubersicht/checkpr.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.widget/resources/status.png
 else 
     #rm -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.png
     cp -f ~/Ubersicht/checkpr.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.widget/resources/status.png
