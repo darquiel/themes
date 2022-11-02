ad3ff7e1b96b2bc2aca0602c7a3d36f925a675c7
mja <mark.andrews@serverobotics.com>
2022-11-02 12:30:56 -0700

    Modifications to complete the rename process.

--- checkpr.fw.widget/checkp.sh
+++ checkpr.fw.widget/checkp.sh
@@ -2,7 +2,7 @@
 var_pr=$`/opt/homebrew/bin/gh pr list -R serve-robotics/skel --search base:release/fleetware/3.33` 
 if [ ${#var_pr} = 1 ] 
 then 
-    cp ~/Ubersicht/checkpr.3.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.3.widget/resources/status.png
+    cp ~/Ubersicht/checkpr.fw.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.fw.widget/resources/status.png
 else 
-    cp -f ~/Ubersicht/checkpr.3.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.3.widget/resources/status.png
+    cp -f ~/Ubersicht/checkpr.fw.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.fw.widget/resources/status.png
 fi
--- checkpr.fw.widget/index.coffee
+++ checkpr.fw.widget/index.coffee
@@ -6,13 +6,13 @@
 
 # Any web image format -- including animated GIFs -- will work.
 
-command: "'checkpr.3.widget/checkp.sh'"
+command: "'checkpr.fw.widget/checkp.sh'"
 
 refreshFrequency: '60000'
 
 render: (output)-> """
 <a href='https://github.com/serve-robotics/skel/pulls?q=is%3Apr+is%3Aopen+base%3Arelease%2Ffleetware%2F3.33'>
-<img src="checkpr.3.widget/resources/status.png" style="width:20%"></a>
+<img src="checkpr.fw.widget/resources/status.png" style="width:20%"></a>
 <repos><p>FW 3.33</p></repos>
 """ #                             ^ Change size here.
 
