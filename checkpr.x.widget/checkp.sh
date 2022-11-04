8c16c9ead15bea4f5d1668596d4c80f076e9342d
mja <mark.andrews@serverobotics.com>
2022-11-04 10:15:01 -0700

    Removing extra widget - combined into one.

--- checkpr.x.widget/checkp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-var_pr=$`/opt/homebrew/bin/gh pr list -R serve-robotics/x --search base:release/rover/3.33` 
-if [ ${#var_pr} = 1 ] 
-then 
-    cp ~/Ubersicht/checkpr.x.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.x.widget/resources/status.png
-else 
-    cp -f ~/Ubersicht/checkpr.x.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.x.widget/resources/status.png
-fi
