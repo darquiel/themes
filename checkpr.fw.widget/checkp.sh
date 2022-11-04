5363bd929bfa8f247115d81832ea7e1794c8ff51
mja <mark.andrews@serverobotics.com>
2022-11-04 10:14:02 -0700

    Removing extra widget - combined into one.

--- checkpr.fw.widget/checkp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-var_pr=$`/opt/homebrew/bin/gh pr list -R serve-robotics/skel --search base:release/fleetware/3.33` 
-if [ ${#var_pr} = 1 ] 
-then 
-    cp ~/Ubersicht/checkpr.fw.widget/resources/ghprstatus.0a.png ~/Ubersicht/checkpr.fw.widget/resources/status.png
-else 
-    cp -f ~/Ubersicht/checkpr.fw.widget/resources/ghprstatus.1a.png ~/Ubersicht/checkpr.fw.widget/resources/status.png
-fi
